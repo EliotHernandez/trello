5c2e1dba2a917d7e2978736012fc6f52d1ee546f
Eliot <eliot@artek.es>
2021-11-24 20:36:43 +0000

    Create static lists

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import Header from './components/header/Header';
-import './App.css';
+import './assets/_scss/app.scss';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <div className="container-fluid">
+                <div className="row lists">
+                    <div className="col resourceList">
+                        <h4>Resource</h4>
+                        <div className="tasks">Tarea 1</div>
+                        <div className="tasks">Tarea 2</div>
+                        <div className="tasks">Tarea 3</div>
+                        <div className="addTasks">Añadir Tarea</div>
+                    </div>
+                    <div className="col toDoList">
+                        <h4>To Do</h4>
+                        <div className="tasks">Tarea 5</div>
+                        <div className="tasks">Tarea 6</div>
+                        <div className="tasks">Tarea 7</div>
+                        <div className="addTasks">Añadir Tarea</div>
+                    </div>
+                    <div className="col doingList">
+                        <h4>Doing</h4>
+                        <div className="tasks">Tarea 8</div>
+                        <div className="tasks">Tarea 9</div>
+                        <div className="addTasks">Añadir Tarea</div>
+                    </div>
+                    <div className="col doneList">
+                        <h4>Done</h4>
+                        <div className="tasks">Tarea 10</div>
+                        <div className="addTasks">Añadir Tarea</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
